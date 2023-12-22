2d2e29bbd0d96d07afd0239e624fd88646abdbd1
quinojuan <quinojuan@gmail.com>
2023-12-22 13:46:00 -0300

    boton y estilos

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -3,8 +3,20 @@ import {styles} from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text style={styles.texto}>Calculadora Screen</Text>
+    <View style={ styles.calculadoraContainer }>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+      <View>
+
+{/* Boton */}
+<View style={ styles.boton }>
+  <Text style={ styles.botonTexto }>1</Text>
+</View>
+
+      </View>
+
+
     </View>
   );
 };
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -1,12 +1,38 @@
-import {StyleSheet} from 'react-native';
+import {AppRegistry, StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   fondo: {
     flex: 1,
     backgroundColor: 'black',
   },
-  texto: {
+  calculadoraContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+  },
+  resultado: {
     color: 'white',
-    fontSize: 20
+    fontSize: 60,
+    textAlign: 'right'
+  },
+  resultadoPequeno: {
+    color: '#rgba(255,255,255, 0.5)',
+    fontSize: 30,
+    textAlign: 'right'
+  },
+  boton: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#9b9b9b',
+    borderRadius: 50,
+    justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '400'
   }
 });
