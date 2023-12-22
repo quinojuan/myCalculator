06ef00e050f27822000dfa310f14f1df49495684
quinojuan <quinojuan@gmail.com>
2023-12-22 18:47:27 -0300

    Operadores matematicos

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,14 +1,24 @@
 import { Text, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 import { BotonCalc } from '../components/BotonCalc';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+
+enum Operadores {
+  sumar,
+  restar,
+  multiplicar,
+  dividir,
+}
 
 export const CalculadoraScreen = () => {
   const [numeroAnterior, setNumeroAnterior] = useState('0');
   const [numero, setNumero] = useState('0');
 
+  const ultimaOperacion = useRef<Operadores>();
+
   const limpiar = () => {
     setNumero('0');
+    setNumeroAnterior('0');
   };
 
   const armarNumero = (numeroTexto: string) => {
@@ -40,9 +50,63 @@ export const CalculadoraScreen = () => {
       setNumero('-' + numero);
     }
   };
+
+  // Mi humilde solución fallida
+  // const btnDelete = () => {
+  //   if (numero.length !== 1) {
+  //     setNumero(numero.slice(0, -1));
+  //   } else if (numero.includes('-')) {
+  //     limpiar();
+  //   } else {
+  //     limpiar();
+  //   }
+  // };
+
+  const btnDelete = () => {
+    let negativo = '';
+    let numeroTemp = numero;
+    if (numero.includes('-')) {
+      negativo = '-';
+      numeroTemp = numero.substring(1);
+    }
+    if (numeroTemp.length > 1) {
+      setNumero(negativo + numeroTemp.slice(0, -1));
+    } else {
+      setNumero('0');
+    }
+  };
+
+  const cambiarNumPorAnterior = () => {
+    if (numero.endsWith('.')) {
+      setNumeroAnterior(numero.slice(0, -1));
+    } else {
+      setNumeroAnterior(numero);
+    }
+    setNumero('0');
+  };
+
+  const btnDividir = () => {
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.dividir;
+  };
+  const btnMultiplicar = () => {
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.multiplicar;
+  };
+  const btnRestar = () => {
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.restar;
+  };
+  const btnSumar = () => {
+    cambiarNumPorAnterior();
+    ultimaOperacion.current = Operadores.sumar;
+  };
+
   return (
     <View style={styles.calculadoraContainer}>
-      <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
+      {numeroAnterior !== '0' && (
+        <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
+      )}
       <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>
         {numero}
       </Text>
@@ -51,29 +115,29 @@ export const CalculadoraScreen = () => {
       <View style={styles.fila}>
         <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
         <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo} />
-        <BotonCalc texto="DEL" color="#9B9B9B" accion={limpiar} />
-        <BotonCalc texto="/" color="#FF9427" accion={limpiar} />
+        <BotonCalc texto="DEL" color="#9B9B9B" accion={btnDelete} />
+        <BotonCalc texto="/" color="#FF9427" accion={btnDividir} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
         <BotonCalc texto="7" accion={armarNumero} />
         <BotonCalc texto="8" accion={armarNumero} />
         <BotonCalc texto="9" accion={armarNumero} />
-        <BotonCalc texto="x" color="#FF9427" accion={limpiar} />
+        <BotonCalc texto="x" color="#FF9427" accion={btnMultiplicar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
         <BotonCalc texto="4" accion={armarNumero} />
         <BotonCalc texto="5" accion={armarNumero} />
         <BotonCalc texto="6" accion={armarNumero} />
-        <BotonCalc texto="-" color="#FF9427" accion={limpiar} />
+        <BotonCalc texto="-" color="#FF9427" accion={btnRestar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
         <BotonCalc texto="1" accion={armarNumero} />
         <BotonCalc texto="2" accion={armarNumero} />
         <BotonCalc texto="3" accion={armarNumero} />
-        <BotonCalc texto="+" color="#FF9427" accion={limpiar} />
+        <BotonCalc texto="+" color="#FF9427" accion={btnSumar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
