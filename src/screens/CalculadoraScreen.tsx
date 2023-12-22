40e55f1f82da2b8e5b3c6fdb8e18d8967df608f4
quinojuan <quinojuan@gmail.com>
2023-12-22 16:56:56 -0300

    reglas de validacion para el 0

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -11,52 +11,75 @@ export const CalculadoraScreen = () => {
     setNumero('0');
   };
 
-  const armarNumero = ( numeroTexto: string ) => {
-    setNumero( numero + numeroTexto)
-  }
+  const armarNumero = (numeroTexto: string) => {
+    // No aceptar doble punto
+    if (numero.includes('.') && numeroTexto === '.') return;
 
+    if (numero.startsWith('0') || numero.startsWith('-0')) {
+      // Punto decimal
+      if (numeroTexto === '.') {
+        setNumero(numero + numeroTexto);
+      } else if (numeroTexto === '0' && numero.includes('.')) {
+        setNumero(numero + numeroTexto);
+
+        // Evaluar si es diferente de cero y no tiene un punto
+      } else if (numeroTexto !== '0' && !numero.includes('.')) {
+        setNumero(numeroTexto);
+      } else if (numeroTexto === '0' && !numero.includes('.')) {
+        setNumero(numero);
+      }
+    } else {
+      setNumero(numero + numeroTexto);
+    }
+  };
+
+  const positivoNegativo = () => {
+    if (numero.includes('-')) {
+      setNumero(numero.replace('-', ''));
+    } else {
+      setNumero('-' + numero);
+    }
+  };
   return (
     <View style={styles.calculadoraContainer}>
       <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
-      <Text 
-        style={styles.resultado}
-        numberOfLines={1}
-        adjustsFontSizeToFit  
-      >{numero}</Text>
+      <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>
+        {numero}
+      </Text>
 
       {/* Fila de botones */}
       <View style={styles.fila}>
         <BotonCalc texto="C" color="#9B9B9B" accion={limpiar} />
-        <BotonCalc texto="+/-" color="#9B9B9B" accion={limpiar} />
+        <BotonCalc texto="+/-" color="#9B9B9B" accion={positivoNegativo} />
         <BotonCalc texto="DEL" color="#9B9B9B" accion={limpiar} />
-        <BotonCalc texto="/" color="#FF9427"  accion={limpiar} />
+        <BotonCalc texto="/" color="#FF9427" accion={limpiar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
-        <BotonCalc texto="7" accion={ armarNumero } />
-        <BotonCalc texto="8" accion={ armarNumero } />
-        <BotonCalc texto="9" accion={ armarNumero } />
+        <BotonCalc texto="7" accion={armarNumero} />
+        <BotonCalc texto="8" accion={armarNumero} />
+        <BotonCalc texto="9" accion={armarNumero} />
         <BotonCalc texto="x" color="#FF9427" accion={limpiar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
-        <BotonCalc texto="4" accion={ armarNumero } />
-        <BotonCalc texto="5" accion={ armarNumero } />
-        <BotonCalc texto="6" accion={ armarNumero } />
+        <BotonCalc texto="4" accion={armarNumero} />
+        <BotonCalc texto="5" accion={armarNumero} />
+        <BotonCalc texto="6" accion={armarNumero} />
         <BotonCalc texto="-" color="#FF9427" accion={limpiar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
-        <BotonCalc texto="1" accion={ armarNumero } />
-        <BotonCalc texto="2" accion={ armarNumero } />
-        <BotonCalc texto="3" accion={ armarNumero } />
+        <BotonCalc texto="1" accion={armarNumero} />
+        <BotonCalc texto="2" accion={armarNumero} />
+        <BotonCalc texto="3" accion={armarNumero} />
         <BotonCalc texto="+" color="#FF9427" accion={limpiar} />
       </View>
       {/* Fila de botones */}
       <View style={styles.fila}>
         {/* poner solo ancho es lo mismo que ancho = true */}
-        <BotonCalc texto="0" accion={ armarNumero } ancho />
-        <BotonCalc texto="." accion={ armarNumero } />
+        <BotonCalc texto="0" accion={armarNumero} ancho />
+        <BotonCalc texto="." accion={armarNumero} />
         <BotonCalc texto="=" color="#FF9427" accion={limpiar} />
       </View>
     </View>
